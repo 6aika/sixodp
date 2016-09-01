b627d9333d02e8b8210be2faacc9dc709a62c261
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2016-09-01 10:44:41 +0300

    add js + css for dropdown and add ripple-effect for buttons

--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -1,6 +1,13 @@
-const Ripplr = require('ripplr');
+$(function() {
 
-Ripplr({
-  selector: '.btn',
-  effectClass: 'ripplr'
+  var Ripplr = require('ripplr');
+
+  Ripplr({
+    selector: '.btn',
+    effectClass: 'ripplr'
+  });
+
+  $('body').on('click', '.dropdown-toggle', function() {
+    $('.dropdown-menu').toggleClass('open');
+  });
 });
