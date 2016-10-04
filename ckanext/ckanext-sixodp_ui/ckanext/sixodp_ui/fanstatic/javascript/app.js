c8f43053ccbd3b4db461066fdb71a4c30e6a0233
Mikko Nieminen <mikko.nieminen@gofore.com>
2016-10-04 15:34:31 +0300

    Change navbar-brand to the center. Add second fixed navbar in mobile size.

--- ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
+++ ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
@@ -16,4 +16,25 @@ $(function() {
   });
 });
 
+
+$(function ($) {
+  $(document).ready(function(){
+
+  // hide .navbar first
+    $('.navbar-fixed-top').hide();
+
+    // fade in .navbar
+    $(function () {
+      $(window).scroll(function () {
+              // set distance user needs to scroll before we fadeIn navbar
+        if ($(this).scrollTop() > 300) {
+          $('.navbar-fixed-top').fadeIn();
+        } else {
+          $('.navbar-fixed-top').fadeOut();
+        }
+      });
+    });
+  });
+}(jQuery));
+
 },{}]},{},[1]);
--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -14,3 +14,24 @@ $(function() {
     }
   });
 });
+
+
+$(function ($) {
+  $(document).ready(function(){
+
+  // hide .navbar first
+    $('.navbar-fixed-top').hide();
+
+    // fade in .navbar
+    $(function () {
+      $(window).scroll(function () {
+              // set distance user needs to scroll before we fadeIn navbar
+        if ($(this).scrollTop() > 300) {
+          $('.navbar-fixed-top').fadeIn();
+        } else {
+          $('.navbar-fixed-top').fadeOut();
+        }
+      });
+    });
+  });
+}(jQuery));
