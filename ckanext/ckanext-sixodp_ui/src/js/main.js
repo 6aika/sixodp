590971ab60966e675d38a17ccd9e612d6feb26c6
Mikko Nieminen <mikko.nieminen@gofore.com>
2016-12-30 12:52:21 +0200

    Add datepicker to all inputs with type as date

--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -15,17 +15,20 @@ $(function() {
   });
 });
 
-
 $(function ($) {
   $(document).ready(function(){
 
-  // hide .navbar first
+    jQuery('input[type=date]').datepicker({
+      format: 'yyyy-mm-dd'
+    });
+
+    // hide .navbar first
     $('.navbar-fixed-top').hide();
 
     // fade in .navbar
     $(function () {
       $(window).scroll(function () {
-              // set distance user needs to scroll before we fadeIn navbar
+        // set distance user needs to scroll before we fadeIn navbar
         if ($(this).scrollTop() > 300) {
           $('.navbar-fixed-top').fadeIn();
         } else {
