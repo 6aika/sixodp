3d6a9ac53d420b29400d782479cac5ad543ed913
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-01-20 15:45:11 +0200

    Remove disabled attributes from form buttons since CKAN relies on button name. Autofill field-file-size -field on submit.

--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -41,23 +41,24 @@ $(function ($) {
       });
     });
 
-    $( "#resource-edit" ).submit(function() {
+    $("form button[type=submit]").one('click', function() {
+      $(this).append(' ').append($('<span id="loading-indicator" ' +
+          'class="icon icon-spinner icon-spin"></span>') );
+    });
+
+    $( "#resource-edit" ).one('submit', function() {
       var fileInput = $('#resource-edit input:file').get(0);
       if(fileInput.files.length > 0) {
-        var fileSize = fileInput.files[0].size;
-        var html = $('<div class="upload-times"><ul>' +
-          '<li>24/1 Mbit/s: ' + Math.ceil(fileSize / 125000 / 60) + ' min</li>' +
-          '<li>10/10 Mbit/s: ' + Math.ceil(fileSize / 1250000 / 60) + ' min</li>' +
-          '<li>100/100 Mbit/s: ' + Math.ceil(fileSize / 12500000 / 60) + ' min</li>' +
-          '</ul></div>');
+          var fileSize = fileInput.files[0].size;
+          $('#field-file_size').val(fileSize);
+          var html = $('<div class="upload-times"><ul>' +
+              '<li>24/1 Mbit/s: ' + Math.ceil(fileSize / 125000 / 60) + ' min</li>' +
+              '<li>10/10 Mbit/s: ' + Math.ceil(fileSize / 1250000 / 60) + ' min</li>' +
+              '<li>100/100 Mbit/s: ' + Math.ceil(fileSize / 12500000 / 60) + ' min</li>' +
+              '</ul></div>');
 
-        $("#submit-info").append(html).show();
+          $("#submit-info").append(html).show();
       }
-      $('button').prop('disabled', true);
-
-      // Due to CKAN's strange way of not identifying elements properly we need to select the correct submit
-      // button with class and name
-      $('.btn-primary[name="save"]').append(' ').append($('<span class="icon icon-spinner icon-spin"></span>') );
     });
   });
 }(jQuery));
