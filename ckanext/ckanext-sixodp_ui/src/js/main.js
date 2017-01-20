0be09bf894852574c826d1f9c3b6cfd0b28ab517
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-01-20 14:55:42 +0200

    Add a loading icon and some info text to resource upload

--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -40,5 +40,24 @@ $(function ($) {
         }
       });
     });
+
+    $( "#resource-edit" ).submit(function() {
+      var fileInput = $('#resource-edit input:file').get(0);
+      if(fileInput.files.length > 0) {
+        var fileSize = fileInput.files[0].size;
+        var html = $('<div class="upload-times"><ul>' +
+          '<li>24/1 Mbit/s: ' + Math.ceil(fileSize / 125000 / 60) + ' min</li>' +
+          '<li>10/10 Mbit/s: ' + Math.ceil(fileSize / 1250000 / 60) + ' min</li>' +
+          '<li>100/100 Mbit/s: ' + Math.ceil(fileSize / 12500000 / 60) + ' min</li>' +
+          '</ul></div>');
+
+        $("#submit-info").append(html).show();
+      }
+      $('button').prop('disabled', true);
+
+      // Due to CKAN's strange way of not identifying elements properly we need to select the correct submit
+      // button with class and name
+      $('.btn-primary[name="save"]').append(' ').append($('<span class="icon icon-spinner icon-spin"></span>') );
+    });
   });
 }(jQuery));
