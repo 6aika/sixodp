ce45eadb76363a056f9f20617df44f1c4d3301ad
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-01-27 15:18:07 +0200

    WP: modify the main search component a bit

--- sixodp/partials/hero.php
+++ sixodp/partials/hero.php
@@ -10,28 +10,25 @@
       <h1 class="heading text-center">6Aika Open Data Portal</h1>
       <h3 class="subheading text-center">Avointa dataa vapaasti hyödynnettäväksesi</h3>  
 
-      <div class="col-md-8 col-sm-12">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="input-group">
-              <input type="text" class="form-control input-lg" aria-label="...">
-              <div class="input-group-btn">
-                <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
-                <ul class="dropdown-menu dropdown-menu-right">
-                  <li><a href="#">Tietoaineistot</a></li>
-                  <li><a href="#">Sovellukset</a></li>
-                  <li><a href="#">Artikkelit</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Muut</a></li>
-                </ul>
-              </div><!-- /btn-group -->
-            </div><!-- /input-group -->
-          </div>
-
-          <div class="col-md-8">
-            <input type="text" class="form-control input-lg" aria-label="...">
-          </div>
-        </div>
+      <div class="col-md-8 col-md-offset-2">
+        <div class="input-group">
+          <div class="input-group-btn">
+            <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Tietoaineistot <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-right">
+              <li><a href="#">Tietoaineistot</a></li>
+              <li><a href="#">Sovellukset</a></li>
+              <li><a href="#">Artikkelit</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Muut</a></li>
+            </ul>
+          </div><!-- /btn-group -->
+          <input type="text" class="form-control input-lg" aria-label="...">
+          <span class="input-group-btn">
+            <button class="btn btn-lg btn-default" type="button">Hae</button>
+          </span>
+        </div><!-- /input-group -->    
       </div>
 
     </div>
