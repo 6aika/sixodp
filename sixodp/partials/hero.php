5f4cd30e11083e2242b36daeb638d13afb964430
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-01-30 13:33:54 +0200

    WP: change search buttons to primary

--- sixodp/partials/hero.php
+++ sixodp/partials/hero.php
@@ -13,7 +13,7 @@
       <div class="col-md-8 col-md-offset-2">
         <div class="input-group">
           <div class="input-group-btn">
-            <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button type="button" class="btn btn-primary btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <span id="selected-domain" data-value="/data/dataset">Tietoaineistot</span> <span class="caret"></span>
             </button>
             <ul class="dropdown-menu dropdown-menu-right">
@@ -27,7 +27,7 @@
           </div><!-- /btn-group -->
           <input type="text" id="q" class="form-control input-lg" aria-label="...">
           <span class="input-group-btn">
-            <button id="search" class="btn btn-lg btn-default" type="button">Hae</button>
+            <button id="search" class="btn btn-lg btn-primary" type="button">Hae</button>
           </span>
         </div><!-- /input-group -->    
       </div>
