1cb5c519b700dd4ed73f5d3081e71408e556d0da
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-02-02 13:27:40 +0200

    Frontpage: hero layout with categories and stats

--- sixodp/partials/categories.php
+++ sixodp/partials/categories.php
@@ -11,55 +11,55 @@
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/hallinto.svg">
-            Hallinto
+            <span class="category__name">Hallinto</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/liikenne.svg">
-            Liikenne ja kartat
+            <span class="category__name">Liikenne ja kartat</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/matkailu.svg">
-            Matkailu
+            <span class="category__name">Matkailu</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/sosiaalipalvelut.svg">
-            Opetus
+            <span class="category__name">Opetus</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/rakentaminen.svg">
-            Asuminen/rakentaminen
+            <span class="category__name">Asuminen/rakentaminen</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/talousjaverotus.svg">
-            Talous
+            <span class="category__name">Talous</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/terveys.svg">
-            Terveys
+            <span class="category__name">Terveys</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/vaesto.svg">
-            Väestö
+            <span class="category__name">Väestö</span>
           </a>
         </div>
         <div class="category">
           <a href="<?php echo site_url(); ?>" class="category__link">
             <img class="category__icon" src="<?php echo site_url(); ?>/wp-content/themes/sixodp/images/icons/ymparistojaluonto.svg">
-            Ympäristö ja liikunta
+            <span class="category__name">Ympäristö ja liikunta</span>
           </a>
         </div>
       </div>
