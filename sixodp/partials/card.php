1bf01f0b063d77554b97169985c9adc25eca26bb
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-02-03 08:37:43 +0200

    Frontpage: add horizontal accordion slider whatever component draft

--- sixodp/partials/card.php
+++ sixodp/partials/card.php
@@ -4,11 +4,9 @@
   *
   */
 ?>
-<div class="card__container">
-  <div class="card">
-    <a href="<?php echo site_url(); ?>" class="card__img--link">
-      <img class="card__img" src="<?php echo $imgUrl; ?>" alt="thumbnail">
-    </a>
+<div class="<?php echo $class; ?>">
+  <div class="card" style="background-image: url(<?php echo $imgUrl; ?>);">
+    <a href="<?php echo site_url(); ?>" class="card__img--link"></a>
     <div class="card__content">
       <h4 class="card__title">
         <a class="card__link" href="<?php echo site_url(); ?>">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</a>
--- sixodp/partials/featured-posts.php
+++ sixodp/partials/featured-posts.php
@@ -4,15 +4,19 @@
   */
 ?>
 
-<div class="wrapper">
+<div class="wrapper--posts">
   <div class="container">
     <div class="row">
-      <h3 class="heading">Ajankohtaista</h3>
       <div class="cards">
         <?php
           $i = 0;
           while($i < 4) {
             $imgUrl = site_url()."/wp-content/themes/sixodp/images/article_bg".($i+1).".jpg";
+            if ($i==2) {
+              $class = 'card__container card__container--active';
+            } else {
+              $class = 'card__container';
+            }
             include(locate_template( 'partials/card.php' ));
             $i++;
           }
