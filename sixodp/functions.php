c656e1b4da707f4e2fd196c1dca13fed30d5bd34
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-02-09 14:11:28 +0200

    Frontpage: add showcase markup and styling with mock data + fix button styling

--- sixodp/functions.php
+++ sixodp/functions.php
@@ -169,13 +169,11 @@ function get_ckan_categories() {
 *  }
 */
 function get_ckan_package_rating($package_id) {
-  /*$data = get_ckan_data(CKAN_API_URL.'/action/rating_showcase_get?package_id='.$package_id);
+  $data = get_ckan_data(CKAN_API_URL.'/action/rating_showcase_get?package_id='.$package_id);
   $rating = ($package_id*0.5);
   if ( $rating > 5 ) {
     $rating = 5;
-  }*/
-  $package_id = 3;
-  $rating = 5;
+  }
   return array('rating' => $rating, 'ratings_count' => ($package_id*2)); //return $data['result'];
 }
 
