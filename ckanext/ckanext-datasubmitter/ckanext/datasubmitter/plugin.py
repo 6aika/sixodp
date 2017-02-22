a6e641910472a89e54bb3b29af196768208c6d6d
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-02-22 16:28:58 +0200

    Update ckanext-datasubmitter to use ITranslation interface

--- ckanext/ckanext-datasubmitter/ckanext/datasubmitter/plugin.py
+++ ckanext/ckanext-datasubmitter/ckanext/datasubmitter/plugin.py
@@ -1,11 +1,14 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class DatasubmitterPlugin(plugins.SingletonPlugin):
+class DatasubmitterPlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IConfigurable)
     plugins.implements(plugins.IRoutes, inherit=True)
+    if toolkit.check_ckan_version(min_version='2.5.0'):
+        plugins.implements(plugins.ITranslation, inherit=True)
 
     # IConfigurer
 
