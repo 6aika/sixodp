04b04821456e092d113e1f2fa347e2b5e8818ed3
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-02-23 13:32:39 +0200

    Set up translations for ckanext-sixodp_scheming

--- ckanext/ckanext-sixodp_scheming/setup.py
+++ ckanext/ckanext-sixodp_scheming/setup.py
@@ -79,5 +79,15 @@ setup(
     entry_points='''
         [ckan.plugins]
         sixodp_scheming=ckanext.sixodp_scheming.plugin:Sixodp_SchemingPlugin
+
+        [babel.extractors]
+        ckan = ckan.lib.extract:extract_ckan
     ''',
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    }
 )
