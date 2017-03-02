a5f8b577deea716ec10fc8a5466ae4ae797c7177
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-03-02 13:31:32 +0200

    CMS: change roadmap list when clicking on roadmap navigation tabs or whatever

--- sixodp/app.js
+++ sixodp/app.js
@@ -31,4 +31,12 @@ var App = {
     var domain = $('#selected-domain').data('value');
     App.search(domain, q);
   });
+
+  $('.nav-link--roadmap').on('click', function() {
+    $list = $(this).attr('data-toggle');
+    $(this).parent().parent().find('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    $('.fluidtable__row').css('display', 'none');
+    $('.fluidtable__row[data-list-id="'+$list+'"]').removeAttr('style');
+  });
 })( jQuery );
