6e32729974d735ab501a507ed5df171fbb1913e5
Panu Kouri <panu.kouri@luonadesign.com>
2017-03-06 18:12:33 +0100

    Choose organization to filter data (initial version).

--- ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/templates/stats/stats.js
+++ ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/templates/stats/stats.js
@@ -4,40 +4,43 @@ var stats = {
 
 stats.init = function () {
   var self = this
-  self.downloadAll(function() {
-    datavis1.init(self.downloads.data1)
-    datavis2.init(self.downloads.data1)
+
+  self.download(
+    'datasets',
+    'current_package_list_with_resources?limit=1000',
+    function () {
+      self.download(
+        'organizations',
+        'organization_list?all_fields=true&include_extras=true',
+        function () {
+
+          // Init all data visualizations
+          datavis1.init(self.downloads.datasets)
+          datavis2.init(self.downloads.datasets, self.downloads.organizations)
+        }
+      )}
+  )
+}
+
+// url = api + 'package_search?rows=20'
+
+stats.download = function (name, endPoint, callback) {
+  var self = this
+
+  url = api + endPoint
+  d3.json(url)
+  // .header('Authorization', apiKey)
+  .get(function (error, response) {
+    console.log('Received JSON from URL', url, 'error:', error, 'response:', response)
+    if (error) {
+      console.log('Error in CKAN API call', url)
+      self.downloads[name] = []
+      throw error
+      callback()
+    }
+    self.downloads[name] = response.result
+    callback()
   })
 }
-
-stats.downloadAll = function (callback) {
-  var self = this
-  // url = api + 'package_search?rows=20'
-  url = api + 'current_package_list_with_resources?limit=100'
-    d3.json(url)
-    // .header('Authorization', apiKey)
-    .get(function (error, response) {
-      console.log('Received JSON from URL', url, 'error:', error, 'response:', response)
-
-      if (error) {
-        console.log('Error in CKAN API call', url)
-
-        self.downloads.data1 = []
-        throw error
-        return false
-      }
-      self.downloads.data1 = response.result
-      callback()
-    })
-}
-
-  // DATA PREPROCESSING
-
-
-
-// render: function () {
-//   datavis2.render()
-// }
-
 
 stats.init()
