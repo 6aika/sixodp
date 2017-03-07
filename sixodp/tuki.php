f1fc9963afe475238032c49978bd3fe9923173ac
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-03-07 08:42:53 +0200

    CMS: add tuki -sivu

---
+++ sixodp/tuki.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * Template Name: Tuki
+ *
+ * @package WordPress
+ * @subpackage Sixodp
+ */
+
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main site-main--home" role="main">
+		<?php
+
+      echo "<h1>Tukisivu</h1>";
+
+		?>
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+<?php get_footer(); ?>
