ba5bf7d5ad3c0926dcd72db46e794142ea1d40bb
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-03-08 14:50:08 +0200

    Add a basic structure for ckanext-statistics extension and enable it

---
+++ ckanext/ckanext-statistics/ckanext/statistics/plugin.py
@@ -0,0 +1,28 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class StatisticsPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'statistics')
+        toolkit.add_resource('public/js/', 'statistics_js')
+
+    # IRoutes
+
+    def before_map(self, map):
+        map.connect('/statistics',
+                    controller='ckanext.statistics.controller:StatisticsController',
+                    action='statistics_read')
+
+        map.connect('/statistics/internal',
+                    controller='ckanext.statistics.controller:StatisticsController',
+                    action='reports_read')
+
+        return map
