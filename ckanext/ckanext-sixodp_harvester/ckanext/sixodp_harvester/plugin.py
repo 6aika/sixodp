1943e0fb63b32938ea358b4a470ab6bb168e649d
Jari Voutilainen <jari.voutilainen@iki.fi>
2017-03-20 16:38:27 +0200

    add csw harvesting

---
+++ ckanext/ckanext-sixodp_harvester/ckanext/sixodp_harvester/plugin.py
@@ -0,0 +1,28 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+from ckanext.spatial.interfaces import ISpatialHarvester
+import logging
+
+log = logging.getLogger(__name__)
+
+
+
+class Sixodp_HarvesterPlugin(plugins.SingletonPlugin):
+    plugins.implements(ISpatialHarvester, inherit=True)
+
+    def get_package_dict(self, context, data_dict):
+
+        package_dict = data_dict['package_dict']
+        package_dict.pop('tags')
+
+        package_dict['date_released'] = "2017-03-20"
+        package_dict['date_updated'] = "2017-03-20"
+        package_dict['license_id'] = "CC-BY-4.0"
+        package_dict['geographical_coverage'] = "tampere"
+        package_dict['keywords'] = {"fi": ["moi"] }
+
+        log.debug(package_dict)
+
+        return package_dict
+
