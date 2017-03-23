632edb81299c339f72058e3c40159c38241ffd6b
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-03-23 15:11:57 +0200

    CMS: add author tpl + styles

--- sixodp/partials/content.php
+++ sixodp/partials/content.php
@@ -9,4 +9,20 @@
   <article class="article" role="article">
     <?php the_content(); ?>
   </article>
+  <div class="article__footer">
+    <div class="author">
+      <div class="author__img">
+        <?php echo get_avatar( get_the_author_meta('user_email'), $size = '50'); ?>  
+      </div>
+      <div class="author__body">
+        <p class="author__text">Kirjoittaja</p>
+        <p class="author__name">
+          <?php the_author(); ?>
+        </p>
+        <p class="post__meta">
+          <?php the_date(); ?>
+        </p>
+      </div>
+    </div>
+  </div>
 </div>
