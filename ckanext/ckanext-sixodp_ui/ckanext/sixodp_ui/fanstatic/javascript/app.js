29fb5112dce041347db1557544f98e886f028d2e
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-03-24 09:24:53 +0200

    Add a close preview -button to showcase horizaccordion

--- ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
+++ ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
@@ -97,6 +97,17 @@ $(function ($) {
             modal.style.display = "none";
         }
     });
+
+    // Toggle horizaccordion collapse button text
+    $("#horizaccordion-collapse-btn").click(function() {
+        if ( $(this).hasClass("collapsed") ) {
+            $(this).find("#show-text").hide();
+            $(this).find("#close-text").show();
+            return;
+        }
+        $(this).find("#show-text").show();
+        $(this).find("#close-text").hide();
+    });
   });
 }(jQuery));
 
