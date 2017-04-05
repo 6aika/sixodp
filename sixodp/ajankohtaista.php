98791f04df30ca49d8cef131e38a9572ed6bf914
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-04-05 13:56:48 +0300

    CMS: add some padding around the tweet card content

--- sixodp/ajankohtaista.php
+++ sixodp/ajankohtaista.php
@@ -21,7 +21,8 @@ get_header(); ?>
 
       echo '<div class="container"><h1 class="heading--main">Ajankohtaista</h1></div>';
       get_template_part( 'partials/horizaccordion' );
-      get_template_part( 'partials/ajankohtaista-links'); 
+      get_template_part( 'partials/ajankohtaista-links');
+      echo '<div class="wrapper" style="padding-bottom: 0;"><div class="container"><h1 class="heading--main" style="margin-bottom:-20px;">Twitter</h1></div></div>';
       echo do_shortcode('[custom-twitter-feeds num=8 class="twitterfeed" screenname="tomdale"]');
 
     ?>
