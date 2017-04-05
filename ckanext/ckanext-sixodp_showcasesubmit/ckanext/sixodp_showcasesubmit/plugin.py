23901f0f0ce2a2d169686473a5b9df9f8e653c66
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-04-05 16:30:27 +0300

    Add basic structure and templates for public showcase form extension

---
+++ ckanext/ckanext-sixodp_showcasesubmit/ckanext/sixodp_showcasesubmit/plugin.py
@@ -0,0 +1,46 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class Sixodp_ShowcasesubmitPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.IRoutes, inherit=True)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'sixodp_showcasesubmit')
+
+    # IConfigurable
+
+    def configure(self, config):
+        # Raise an exception if required configs are missing
+        required_keys = (
+            'ckanext.sixodp_showcasesubmit.creating_user_username',
+        )
+
+        for key in required_keys:
+            if config.get(key) is None:
+                raise RuntimeError(
+                    'Required configuration option {0} not found.'.format(
+                        key
+                    )
+                )
+
+    # IRoutes
+
+    def before_map(self, map):
+        map.connect('/submit-showcase',
+                    controller='ckanext.sixodp_showcasesubmit.controller:Sixodp_ShowcasesubmitController',
+                    action='index',
+                    conditions=dict(method=['GET']))
+
+        map.connect('/submit-showcase',
+                    controller='ckanext.sixodp_showcasesubmit.controller:Sixodp_ShowcasesubmitController',
+                    action='submit',
+                    conditions=dict(method=['POST']))
+
+        return map
---
+++ ckanext/ckanext-sixodp_showcasesubmit/ckanext/sixodp_showcasesubmit/tests/test_plugin.py
@@ -0,0 +1,5 @@
+"""Tests for plugin.py."""
+import ckanext.sixodp_showcasesubmit.plugin as plugin
+
+def test_plugin():
+    pass
