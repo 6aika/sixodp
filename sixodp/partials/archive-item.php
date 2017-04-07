a6607199494eb8174ae636a3256e48996e09b8b2
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-04-07 12:30:10 +0300

    Shared styles: heavier use of vars

--- sixodp/partials/archive-item.php
+++ sixodp/partials/archive-item.php
@@ -3,13 +3,13 @@
   * Archive item
   */
 ?>
-<div class="row" style="border-bottom: 1px solid #e7e7e7; padding: 40px 0;">
+<div class="row archive-item">
   <div class="col-md-12">
-    <span class="meta" style="font-size: 12px;"><?php the_date(); ?></span>
-    <h3 class="heading" style="margin-top: 10px;">
+    <span class="archive-item__meta"><?php the_date(); ?></span>
+    <h3 class="archive-item__title">
       <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h3>
-    <div class="excerpt">
+    <div class="archive-item__excerpt">
       <?php the_excerpt(); ?>
     </div>
   </div>
