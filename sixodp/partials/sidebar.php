10325b00f5dac699c7ada5e412a939eb13f32b5b
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-04-10 09:27:11 +0300

    CMS: add dynamic sidebar to tukichild pages

--- sixodp/partials/sidebar.php
+++ sixodp/partials/sidebar.php
@@ -8,40 +8,56 @@
   <div class="sidebar__wrapper">
     <ul class="sidebar__list">
       <li class="sidebar__item--heading">
-        <i class="material-icons">camera</i> Kuinka aloitan?
+        <?php echo get_the_category()[0]->cat_name; ?>
       </li>
       <li class="sidebar__item">
-        <a href="#" class="sidebar__link--active">Mitä on avoin data?</a>
+        <a href="#" class="sidebar__link--active"><?php the_title(); ?></a>
       </li>
-      <li class="sidebar__item">
-        <a href="#" class="sidebar__link">Datajournalismiartikkelien kirjoittaminen</a>
-      </li>
-      <li class="sidebar__item">
-        <a href="#" class="sidebar__link">Näin avaat instanssin</a>
-      </li>
-      <li class="sidebar__item">
-        <a href="#" class="sidebar__link">Koulutusmateriaalit</a>
-      </li>
+      <?php
+
+        $product_page_args = array(
+            'post_type' => 'page',
+            'posts_per_page' => '-1',
+            'child_of' => get_page_by_title('Tuki')->ID,
+            'category_name' => get_the_category()[0]->cat_name,
+            'post__not_in' => array($post->ID)
+        );
+
+        $my_wp_query = new WP_Query();
+        $product_pages = $my_wp_query->query($product_page_args);
+
+        foreach ($product_pages as $product_page){ ?>
+          <li class="sidebar__item--heading">
+            <a href="#" class="sidebar__link">
+              <?php echo $product_page->post_title; ?>  
+            </a>
+          </li><?php
+        }
+        wp_reset_query();
+      ?>
     </ul>
+
+    <?php
+      $categories=get_categories(array(
+        'parent' => get_cat_ID('tuki'), 
+        'exclude' => array(get_the_category()[0]->term_id)
+      ));
+
+      if (count($categories) > 0) {
+    ?>
     <ul class="sidebar__list--heading">
-      <li class="sidebar__item--heading">
-        <a href="#" class="sidebar__link--block">
-        <i class="material-icons">settings</i>
-          Tietoaineistojen käyttäjälle
-          <span class="sidebar__icon-wrapper">
-            <i class="material-icons">arrow_forward</i>
-          </span>
-        </a>
-      </li>
-      <li class="sidebar__item--heading">
-        <a href="#" class="sidebar__link--block">
-        <i class="material-icons">timeline</i>
-          Tietoaineistojen julkaisijalle
-          <span class="sidebar__icon-wrapper">
-            <i class="material-icons">arrow_forward</i>
-          </span>
-        </a>
-      </li>
+      <?php foreach ( $categories as $cat ) : ?>
+        <li class="sidebar__item--heading">
+          <a href="#" class="sidebar__link--block">
+          <i class="material-icons">settings</i>
+            <?php echo $cat->cat_name; ?>
+            <span class="sidebar__icon-wrapper">
+              <i class="material-icons">arrow_forward</i>
+            </span>
+          </a>
+        </li>
+      <?php endforeach; ?>
     </ul>
+    <?php } ?>
   </div>
 </div>
