4b8fab303d98f54af2c7a510460a30c968623231
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-04-10 16:39:51 +0300

    Change date input types to text

--- ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
+++ ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
@@ -21,8 +21,8 @@ $(function ($) {
 
     // Attempt to get the user language, datepicker will default to en-US if not successful
     var language = window.navigator.userLanguage || window.navigator.language;
-    jQuery('input[type=date]').datepicker({
-      format: 'd.m.yyyy',
+    jQuery('.has-datepicker input').datepicker({
+      format: 'yyyy-mm-dd',
       weekStart: 1,
       language: language,
       todayHighlight: true
--- ckanext/ckanext-sixodp_ui/src/js/main.js
+++ ckanext/ckanext-sixodp_ui/src/js/main.js
@@ -20,7 +20,7 @@ $(function ($) {
 
     // Attempt to get the user language, datepicker will default to en-US if not successful
     var language = window.navigator.userLanguage || window.navigator.language;
-    jQuery('input[type=date]').datepicker({
+    jQuery('.has-datepicker input').datepicker({
       format: 'yyyy-mm-dd',
       weekStart: 1,
       language: language,
