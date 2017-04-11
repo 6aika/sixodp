d9967f8e66df0d952dc0e5250dc9bc5bf4f67c54
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-04-11 10:05:18 +0300

    Remove unnecessary sass variables, close showcase horizaccordion when query parameters present

--- ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
+++ ckanext/ckanext-sixodp_ui/ckanext/sixodp_ui/fanstatic/javascript/app.js
@@ -102,16 +102,30 @@ $(function ($) {
         }
     });
 
+    var showOpenHorizaccordionButton = function(horizaccordionElement) {
+      horizaccordionElement.find("#show-text").show();
+      horizaccordionElement.find("#close-text").hide();
+    }
+
+    var showCloseHorizaccordionButton = function(horizaccordionElement) {
+      horizaccordionElement.find("#show-text").hide();
+      horizaccordionElement.find("#close-text").show();
+    }
+
     // Toggle horizaccordion collapse button text
     $("#horizaccordion-collapse-btn").click(function() {
-        if ( $(this).hasClass("collapsed") ) {
-            $(this).find("#show-text").hide();
-            $(this).find("#close-text").show();
-            return;
-        }
-        $(this).find("#show-text").show();
-        $(this).find("#close-text").hide();
+      if ( $(this).hasClass("collapsed") ) {
+          return showCloseHorizaccordionButton($(this));
+      }
+      showOpenHorizaccordionButton($(this));
     });
+
+    // Close horizaccordion if query parameters present
+    if(window.location.search) {
+      $('#horizaccordion').addClass('collapsed');
+      $('#horizaccordion').removeClass('in');
+      showOpenHorizaccordionButton($('#horizaccordion'));
+    }
   });
 }(jQuery));
 
