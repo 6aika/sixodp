aa8d7ed35533c4604f5d5fb2b90c1cc4f5b461d0
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-05-12 08:56:54 +0300

    Fetch extra fields to homepage category list to display title correctly

--- sixodp/functions.php
+++ sixodp/functions.php
@@ -437,7 +437,7 @@ function get_count($type) {
 }
 
 function get_ckan_categories() {
-  $data = get_ckan_data(CKAN_API_URL.'/action/group_list?all_fields=true');
+  $data = get_ckan_data(CKAN_API_URL.'/action/group_list?all_fields=true&include_extras=true');
   return $data['result'];
 }
 
