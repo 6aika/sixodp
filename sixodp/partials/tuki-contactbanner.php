09b8b9b374c3d6b2f002dd8b6d9c12890764a769
Aki Seppänen <aki.seppanen@gofore.com>
2017-06-02 15:54:46 +0300

    add configurable email for contacting support

--- sixodp/partials/tuki-contactbanner.php
+++ sixodp/partials/tuki-contactbanner.php
@@ -2,6 +2,8 @@
   /**
   * Contact box on Tuki frontpage.
   */
+
+$support_user = get_user_by('login', 'support');
 ?>
 
 <div class="wrapper--contactbanner">
@@ -11,8 +13,8 @@
         <div class="contactbanner">
           <i class="material-icons contactbanner__icon--chat">chat</i>
           <div class="row">
-            <h1 class="heading"><?php _e('Any questions?', 'sixodp');?></h1>
-            <button class="btn btn-transparent"><?php _e('Contact us', 'sixodp');?> <i class="material-icons">arrow_forward</i></button>
+            <h1 class="heading"><?php _e('Any questions?');?></h1>
+            <a type="button" href="mailto:<?php echo $support_user->user_email?>" class="btn btn-transparent"><?php _e('Contact us');?> <i class="material-icons">arrow_forward</i></a>
           </div>
         </div>
       </div>
