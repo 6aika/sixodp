b80dd90d39ff524ea08835d52ec15fd5f30b069b
Aki Seppänen <aki.seppanen@gofore.com>
2017-06-15 11:02:46 +0300

    add title field to request forms

--- sixodp/data_request_form.php
+++ sixodp/data_request_form.php
@@ -18,13 +18,14 @@ $name = '';
 
 if (isset($_POST['data_request_submit_form'])) {
   $content = $_POST['data_request_content'];
-  $title = wp_trim_words(strip_tags($_POST['data_request_content']), 30);
+  $title = $_POST['data_request_title'];
   $name = $_POST['data_request_name'];
   $email = $_POST['data_request_email'];
 
   $errors = array();
 
   if (empty($content)) $errors[] = __('Content is required', 'sixodp');
+  if (empty($title)) $errors[] = __('Title is required', 'sixodp');
   if (empty($email)) $errors[] = __('Email is required', 'sixodp');
   else if (is_email($email) === false) $errors[] = __('Email must be valid', 'sixodp');
   if (empty($name) or strlen($name) < 3) $errors[] = __('Name is required and must be over 3 characters long', 'sixodp');
@@ -67,6 +68,8 @@ get_header(); ?>
         }
         ?>
         <form action="" method="POST">
+          <label for="data_request_title"><?php _e('Title', 'sixodp');?></label>
+          <input type="text" name="data_request_title" id="data_request_title" class="form-control" value="<?php echo $name; ?>" />
           <label for="data_request_content"><?php _e('Your request', 'sixodp');?></label>
           <?php
           wp_editor($content, 'data_request_content', array(
