cad102225765bda785275a06e59b499a47917d59
Aki Seppänen <aki.seppanen@gofore.com>
2017-08-15 13:40:29 +0300

    rename category class to prevent conflict on categories page

--- sixodp/partials/categories.php
+++ sixodp/partials/categories.php
@@ -16,7 +16,7 @@
                 $image_url = $category['image_display_url'];
                 $package_count = $category['package_count']; ?>
                 <div class="category__wrapper">
-                  <div class="category">
+                  <div class="category--box">
                     <a href="<?php echo $url; ?>" class="category__link">
                       <img class="category__icon" src="<?php echo $image_url; ?>">
                       <span class="category__name"><?php echo $title; ?></span>
