677fe6cd92189676370dc85843ed70774b8b4e65
Aki Seppänen <aki.seppanen@gofore.com>
2017-08-15 09:59:29 +0300

    layout improvements to tuki pages

--- sixodp/category-tuki.php
+++ sixodp/category-tuki.php
@@ -36,67 +36,66 @@ $category = get_queried_object();
           </h1>
         </div>
 
-        <?php include( locate_template('partials/tuki-sidebar.php') ); ?>
+        <div class="row">
+          <?php include( locate_template('partials/tuki-sidebar.php') ); ?>
 
-        <div class="cards--post">
-        <?php
-        while ( have_posts() ) : the_post(); ?>
-
-        <div class="card--post">
-          <?php
-            if (has_post_thumbnail( $post->ID ) ):
-              $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-            else :
-              $image = array("/assets/images/frontpage.jpg");
-            endif;
-          ?>
-          <a href="<?php the_permalink(); ?>" class="post__img--link" style="background-image: url(<?php echo $image[0]; ?>);"></a>
-          <div class="post__content">
-            <h4 class="post__title">
-              <a class="post__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h4>
-            <div class="post__separator"></div>
-            <div class="post__meta">
-              <span><?php echo parse_date(get_the_date('c')); ?></span>
-
-              <ul>
+          <div class="col-xs-12 col-sm-8 cards--post">
+            <?php
+            while ( have_posts() ) : the_post(); ?>
+              <div class="card--post">
                 <?php
-                  if ( count(get_the_category()) > 0 ) {
-                    foreach ( get_the_category() as $cat ) { ?>
-                      <li><a href="<?php echo get_category_link($cat->cat_ID) ?>"><?php echo $cat->name; ?></a></li><?php
-                    }
-                  }
+                  if (has_post_thumbnail( $post->ID ) ):
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                  else :
+                    $image = array("/assets/images/frontpage.jpg");
+                  endif;
                 ?>
-              </ul>
-            </div>
-            <div class="post__text">
-              <?php 
-              $content = get_the_content();
+                <a href="<?php the_permalink(); ?>" class="post__img--link" style="background-image: url(<?php echo $image[0]; ?>);"></a>
+                <div class="post__content">
+                  <h4 class="post__title">
+                    <a class="post__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                  </h4>
+                  <div class="post__separator"></div>
+                  <div class="post__meta">
+                    <span><?php echo parse_date(get_the_date('c')); ?></span>
 
-              $content = wp_strip_all_tags($content);
-              if (strlen($content) > 200) {
-                $content = substr($content, 0, 180);
-                $content = substr($content, 0, strripos($content, ' '));
-                $content .= '...';
-              }
+                    <ul>
+                      <?php
+                        if ( count(get_the_category()) > 0 ) {
+                          foreach ( get_the_category() as $cat ) { ?>
+                            <li><a href="<?php echo get_category_link($cat->cat_ID) ?>"><?php echo $cat->name; ?></a></li><?php
+                          }
+                        }
+                      ?>
+                    </ul>
+                  </div>
+                  <div class="post__text">
+                    <?php 
+                    $content = get_the_content();
 
-              print $content;
-              ?>
-            </div>
-            <div class="post__footer">
-            </div>
+                    $content = wp_strip_all_tags($content);
+                    if (strlen($content) > 200) {
+                      $content = substr($content, 0, 180);
+                      $content = substr($content, 0, strripos($content, ' '));
+                      $content .= '...';
+                    }
+
+                    print $content;
+                    ?>
+                  </div>
+                  <div class="post__footer">
+                  </div>
+                </div>
+              </div>
+            <?php
+            endwhile;
+            wp_reset_postdata();
+            posts_nav_link();
+            ?>
           </div>
-        </div>
-
-        <?php
-        endwhile;
-        wp_reset_postdata();
-        posts_nav_link();
-        ?>
-        </div>
-      </div>
-    </div>
+        </div><!-- .row -->
+      </div><!-- .container -->
+    </div><!-- .wrapper -->
   </main><!-- .site-main -->
-
 </div><!-- .content-area -->
 <?php get_footer(); ?>
