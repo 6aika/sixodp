1c77342b5b06ae58be1c3f71a6fe34fa1c244fff
Aki Seppänen <aki.seppanen@gofore.com>
2017-08-15 14:37:48 +0300

    improve pagination in categories

--- sixodp/category-tuki.php
+++ sixodp/category-tuki.php
@@ -42,45 +42,54 @@ $category = get_queried_object();
           <?php include( locate_template('partials/tuki-sidebar.php') ); ?>
 
           <div class="col-xs-12 col-sm-8 cards--post">
-            <?php
-            while ( have_posts() ) : the_post(); ?>
-              <div class="card--post">
-                <?php
-                  if (has_post_thumbnail( $post->ID ) ):
-                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-                  else :
-                    $image = array("/assets/images/frontpage.jpg");
-                  endif;
-                ?>
-                <a href="<?php the_permalink(); ?>" class="post__img--link" style="background-image: url(<?php echo $image[0]; ?>);"></a>
-                <div class="post__content">
-                  <h4 class="post__title">
-                    <a class="post__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  </h4>
-                  <div class="post__separator"></div>
-                  <div class="post__meta">
-                    <span><?php echo parse_date(get_the_date('c')); ?></span>
-                  </div>
-                  <div class="post__categories">
-                    <ul>
-                      <?php
-                        if ( count(get_the_category()) > 0 ) {
-                          foreach ( get_the_category() as $cat ) { ?>
-                            <li><a href="<?php echo get_category_link($cat->cat_ID) ?>"><?php echo $cat->name; ?></a></li><?php
+            <div class="cards--post">
+              <?php
+              while ( have_posts() ) : the_post(); ?>
+                <div class="card--post">
+                  <?php
+                    if (has_post_thumbnail( $post->ID ) ):
+                      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                    else :
+                      $image = array("/assets/images/frontpage.jpg");
+                    endif;
+                  ?>
+                  <a href="<?php the_permalink(); ?>" class="post__img--link" style="background-image: url(<?php echo $image[0]; ?>);"></a>
+                  <div class="post__content">
+                    <h4 class="post__title">
+                      <a class="post__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h4>
+                    <div class="post__separator"></div>
+                    <div class="post__meta">
+                      <span><?php echo parse_date(get_the_date('c')); ?></span>
+                    </div>
+                    <div class="post__categories">
+                      <ul>
+                        <?php
+                          if ( count(get_the_category()) > 0 ) {
+                            foreach ( get_the_category() as $cat ) { ?>
+                              <li><a href="<?php echo get_category_link($cat->cat_ID) ?>"><?php echo $cat->name; ?></a></li><?php
+                            }
                           }
-                        }
-                      ?>
-                    </ul>
-                  </div>
-                  <div class="post__footer">
+                        ?>
+                      </ul>
+                    </div>
+                    <div class="post__footer">
+                    </div>
                   </div>
                 </div>
+              <?php
+              endwhile;
+              wp_reset_postdata();
+              ?>
+            </div>
+            <div class="paginate">
+              <div class="paginate-prev">
+                <?php previous_posts_link() ?>
               </div>
-            <?php
-            endwhile;
-            wp_reset_postdata();
-            posts_nav_link();
-            ?>
+              <div class="paginate-next">
+                <?php next_posts_link() ?>
+              </div>
+            </div>
           </div>
         </div><!-- .row -->
       </div><!-- .container -->
