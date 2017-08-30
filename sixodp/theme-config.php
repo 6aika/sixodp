1c6b3d7f7b1b9436b3c64d785d05714b51d4c570
Aki Seppänen <aki.seppanen@gofore.com>
2017-08-30 10:48:24 +0300

    Improve menu provisioning by adding new items

--- sixodp/theme-config.php
+++ sixodp/theme-config.php
@@ -15,12 +15,11 @@
       'code' => 'fi',
       'pages' => array(
         'main' => array( 'post_title' => 'fi', 'post_content' => "This is my post", 'page_template' => 'home.php' ),
-          'roadmap' => array( 'post_title' => 'Roadmap', 'post_content' => "This is my post", 'page_template' => 'roadmap.php' ),
-          'latest_updates' => array( 'post_title' => 'Ajankohtaista', 'post_content' => "This is my post", 'page_template' => 'ajankohtaista.php' ),
-          'support' => array( 'post_title' => 'Tuki', 'post_content' => "This is my post", 'page_template' => 'tuki.php' ),
-          'data_request_form' => array( 'post_title' => 'Uusi datatoive', 'post_content' => "This is my post", 'page_template' => 'data_request_form.php' ),
-          'app_request_form' => array( 'post_title' => 'Uusi sovellustoive', 'post_content' => "This is my post", 'page_template' => 'app_request_form.php' )
-
+        'roadmap' => array( 'post_title' => 'Työn alla', 'post_content' => "This is my post", 'page_template' => 'roadmap.php' ),
+        'latest_updates' => array( 'post_title' => 'Ajankohtaista', 'post_content' => "This is my post", 'page_template' => 'ajankohtaista.php' ),
+        'support' => array( 'post_title' => 'Tuki', 'post_content' => "This is my post", 'page_template' => 'tuki.php' ),
+        'data_request_form' => array( 'post_title' => 'Uusi datatoive', 'post_content' => "This is my post", 'page_template' => 'data_request_form.php' ),
+        'app_request_form' => array( 'post_title' => 'Uusi sovellustoive', 'post_content' => "This is my post", 'page_template' => 'app_request_form.php' )
     ) ),
     array(
       'locale' => 'en_GB',
@@ -101,22 +100,45 @@
   ) );
 
   define( 'PRIMARY_MENU_ITEMS_FI', array(
-    array('menu-item-title' => 'Etusivu', 'menu-item-url' => '/fi'),
-    array('menu-item-title' => 'Tietoaineistot', 'menu-item-url' => '/data/fi/dataset'),
-    array('menu-item-title' => 'Sovellukset', 'menu-item-url' => '/data/fi/showcase'),
-    array('menu-item-title' => 'Datakokoelmat', 'menu-item-url' => '/data/fi/collection')
+    array('menu-item-title' => 'Etusivu', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'fi', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Tietoaineistot', 'menu-item-url' => '/data/fi/dataset/', 'children' => array(
+      array('menu-item-title' => 'Työnalla', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'tyon-alla', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'Uusi datatoive', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'uusi-datatoive', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'Datakokoelmat', 'menu-item-url' => '/data/fi/collection/'),
+    )),
+    array('menu-item-title' => 'Sovellukset', 'menu-item-url' => '/data/fi/showcase', 'children' => array(
+      array('menu-item-title' => 'Ilmoita sovellus', 'menu-item-url' => '/data/fi/submit-showcase/'),
+      array('menu-item-title' => 'Uusi sovellustoive', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'uusi-sovellustoive', 'menu-item-object' => 'page'),
+    )),
+    array('menu-item-title' => 'Ajankohtaista', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'ajankohtaista', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Tuki', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'tuki', 'menu-item-object' => 'page'),
   ) );
+
   define( 'PRIMARY_MENU_ITEMS_EN', array(
-    array('menu-item-title' => 'Frontpage', 'menu-item-url' => '/en_GB'),
-    array('menu-item-title' => 'Datasets', 'menu-item-url' => '/data/en_GB/dataset'),
-    array('menu-item-title' => 'Showcases', 'menu-item-url' => '/data/en_GB/showcase'),
-    array('menu-item-title' => 'Collections', 'menu-item-url' => '/data/en_GB/collection')
+    array('menu-item-title' => 'Frontpage', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'en_gb', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Datasets', 'menu-item-url' => '/data/en_GB/dataset/', 'children' => array(
+      array('menu-item-title' => 'Roadmap', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'roadmap', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'New data request', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'new-data-request', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'Collections', 'menu-item-url' => '/data/en_GB/collection/'),
+    )),
+    array('menu-item-title' => 'Showcases', 'menu-item-url' => '/data/en_GB/showcase', 'children' => array(
+      array('menu-item-title' => 'Submit showcase', 'menu-item-url' => '/data/fi/submit-showcase/'),
+      array('menu-item-title' => 'New app request', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'new-app-request', 'menu-item-object' => 'page'),
+    )),
+    array('menu-item-title' => 'Latest updates', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'latest-updates', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Support', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'support', 'menu-item-object' => 'page'),
   ) );
+
   define( 'PRIMARY_MENU_ITEMS_SV', array(
-    array('menu-item-title' => 'Huvudsida', 'menu-item-url' => '/sv'),
-    array('menu-item-title' => 'Datasets', 'menu-item-url' => '/data/sv/dataset'),
-    array('menu-item-title' => 'Vitrin', 'menu-item-url' => '/data/sv/showcase'),
-    array('menu-item-title' => 'Kollektion', 'menu-item-url' => '/data/sv/collection')
+    array('menu-item-title' => 'Huvudsida', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'en_gb', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Datasets', 'menu-item-url' => '/data/sv/dataset/', 'children' => array(
+      array('menu-item-title' => 'Roadmap', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'roadmap', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'Ny data begäran', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'ny-data-begaran', 'menu-item-object' => 'page'),
+      array('menu-item-title' => 'Kollektion', 'menu-item-url' => '/data/sv/collection/'),
+    )),
+    array('menu-item-title' => 'Vitrin', 'menu-item-url' => '/data/en_GB/showcase'),
+    array('menu-item-title' => 'Senaste uppdateringarna', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'senaste-uppdateringarna', 'menu-item-object' => 'page'),
+    array('menu-item-title' => 'Stöd', 'menu-item-type' => 'post_type', 'menu-item-object-slug' => 'stod', 'menu-item-object' => 'page'),
   ) );
 
   define( 'FOOTER_MENU_ITEMS_FI', array(
