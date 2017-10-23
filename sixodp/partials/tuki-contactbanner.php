2497011897ef72839e3d4d4b9fafb88a1bf16444
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-10-23 15:19:41 +0300

    Update showcase idea templates to include a sidebar, update translations, add missing translation contexts

--- sixodp/partials/tuki-contactbanner.php
+++ sixodp/partials/tuki-contactbanner.php
@@ -13,8 +13,11 @@ $support_user = get_user_by('login', 'support');
         <div class="contactbanner">
           <i class="material-icons contactbanner__icon--chat">chat</i>
           <div class="row">
-            <h1 class="heading"><?php _e('Any questions?');?></h1>
-            <a type="button" href="mailto:<?php echo $support_user->user_email?>" class="btn btn-transparent"><?php _e('Contact us');?> <i class="material-icons">arrow_forward</i></a>
+            <h1 class="heading"><?php _e('Any questions?', 'sixodp');?></h1>
+            <a type="button" href="mailto:<?php echo $support_user->user_email?>"
+               class="btn btn-transparent"><?php _e('Contact us', 'sixodp');?>&nbsp
+              <i class="material-icons">arrow_forward</i>
+            </a>
           </div>
         </div>
       </div>
