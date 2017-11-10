d5cb6f66cb04e07bc71332b45ddbc125ef1af1ad
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-11-10 14:06:26 +0200

    Fix showcase idea and data request archives

--- sixodp/archive-data_request.php
+++ sixodp/archive-data_request.php
@@ -20,33 +20,41 @@ get_header(); ?>
     ?>
 
     <div class="page-hero"></div>
-    <div class="page-content page-hero-content container">
-      <div class="row">
-        <div class="col-md-3 sidebar">
-          <ul>
-            <li class="sidebar-item--highlight">
-              <a href="<?php echo get_permalink(get_translated_page_by_title('Uusi datatoive')); ?>">
-                <?php _e('New data request', 'sixodp') ?>
-                <span class="sidebar-icon-wrapper">
-                  <span class="fa fa-chevron-right"></span>
-                </span>
-              </a>
-            </li>
-          </ul>
+    <div class="page-hero-content container">
+      <div class="wrapper">
+        <div class="headingbar">
+          <h1 class="heading-main"><?php _e('Data Requests', 'sixodp') ?></h1>
         </div>
 
-        <div class="col-md-9">
-          <h1 class="heading--archive"><?php _e('Data Requests', 'sixodp') ?></h1>
-          <?php
-          // Start the loop.
-          while ( have_posts() ) : the_post();
-            // Include the page content template.
-            get_template_part('partials/archive-item');
+        <div class="row">
+          <div class="col-md-3 sidebar">
+            <ul>
+              <li class="sidebar-item--highlight">
+                <a href="<?php echo get_permalink(get_translated_page_by_title('Uusi datatoive')); ?>">
+                  <?php _e('New data request', 'sixodp') ?>
+                  <span class="sidebar-icon-wrapper">
+                    <span class="fa fa-chevron-right"></span>
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
 
-            // End of the loop.
-          endwhile;
-          ?>
+          <div class="col-md-9 col-xs-12">
+            <div class="cards cards--2 cards--image">
+              <?php
+                // Start the loop.
+                while ( have_posts() ) : the_post();
+                  // Include the page content template.
+                  get_template_part('partials/archive-item');
+
+                  // End of the loop.
+                endwhile;
+              ?>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
 
