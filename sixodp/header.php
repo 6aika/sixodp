b4dd7c02d190f9031e1aee51cff352b303a6f6f6
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-12-12 13:21:35 +0200

    Replace both fonts with roboto

--- sixodp/header.php
+++ sixodp/header.php
@@ -21,7 +21,7 @@
 	<?php wp_head(); ?>
 	<script src="https://use.fontawesome.com/4f2a9d3d3d.js"></script>
 	<link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/sixodp/app.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700|Roboto+Slab:300,400,600" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<script type="text/javascript">
 		var locale = '<?php echo get_current_locale() ?>';
