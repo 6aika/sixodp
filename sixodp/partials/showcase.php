ae90d08e37fb57bd51a941309d8f68664cf3fa4e
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-12-18 08:26:23 +0200

    Add rating to frontpage showcase cards

--- sixodp/partials/showcase.php
+++ sixodp/partials/showcase.php
@@ -8,5 +8,19 @@
   <img src="<?php echo $imgUrl; ?>">
   <div class="card-content card-content-slide-up">
     <h4 class="card-title"><?php echo $showcase['title']; ?></h4>
+    <div class="card-title-secondary">
+      <?php
+        $rating = get_ckan_package_rating($packageId)['rating'];
+        $j = $rating['rating'];
+        for ($i = 0; $i < 5; $i++) {
+          if($i < $rating) {
+            echo '<span class="fa fa-star"></span>';
+          }
+          else {
+            echo '<span class="fa fa-star-o"></span>';
+          }
+        }
+      ?>
+    </div>
   </div>
 </a>
