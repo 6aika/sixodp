9e36ec4bad9e4396c9ccb660d5e8b12827150c3f
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-12-20 07:19:54 +0200

    laying out the stats and search of frontpage

--- sixodp/partials/stats.php
+++ sixodp/partials/stats.php
@@ -4,33 +4,31 @@
   */
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="stats">
-      <a href="<?php echo CKAN_BASE_URL; ?>/<?php echo get_current_locale_ckan(); ?>/dataset" class="stat__link">
-        <div class="stat__wrapper">
-          <div class="stat">
-            <div class="stat__value counter"><?php echo get_dataset_count(); ?></div>
-            <div class="stat__description"><?php _e('Datasets', 'sixodp');?></div>
-          </div>
+<div class="row">
+  <div class="stats">
+    <a href="<?php echo CKAN_BASE_URL; ?>/<?php echo get_current_locale_ckan(); ?>/dataset" class="stat__link">
+      <div class="stat__wrapper">
+        <div class="stat">
+          <div class="stat__value counter"><?php echo get_dataset_count(); ?></div>
+          <div class="stat__description"><?php _e('Datasets', 'sixodp');?></div>
         </div>
-      </a>
-      <a href="<?php echo CKAN_BASE_URL; ?>/<?php echo get_current_locale_ckan(); ?>/showcase" class="stat__link">
-        <div class="stat__wrapper">
-          <div class="stat">
-            <div class="stat__value counter"><?php echo get_showcases_count(); ?></div>
-            <div class="stat__description"><?php _e('Applications', 'sixodp');?></div>
-          </div>
+      </div>
+    </a>
+    <a href="<?php echo CKAN_BASE_URL; ?>/<?php echo get_current_locale_ckan(); ?>/showcase" class="stat__link">
+      <div class="stat__wrapper">
+        <div class="stat">
+          <div class="stat__value counter"><?php echo get_showcases_count(); ?></div>
+          <div class="stat__description"><?php _e('Applications', 'sixodp');?></div>
         </div>
-      </a>
-      <a href="<?php echo get_api_link() ?>" class="stat__link">
-        <div class="stat__wrapper">
-          <div class="stat">
-            <div class="stat__value counter"><?php echo get_api_count(); ?></div>
-            <div class="stat__description"><?php _e('Interfaces', 'sixodp');?></div>
-          </div>
+      </div>
+    </a>
+    <a href="<?php echo get_api_link() ?>" class="stat__link">
+      <div class="stat__wrapper">
+        <div class="stat">
+          <div class="stat__value counter"><?php echo get_api_count(); ?></div>
+          <div class="stat__description"><?php _e('Interfaces', 'sixodp');?></div>
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   </div>
 </div>
