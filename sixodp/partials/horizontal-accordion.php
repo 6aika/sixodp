eb74e761b117e69cf518dd0449f2e01a28b5c84d
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2017-12-20 13:43:20 +0200

    update horizaccordion to just render a card

--- sixodp/partials/horizontal-accordion.php
+++ sixodp/partials/horizontal-accordion.php
@@ -4,53 +4,46 @@
  */
 ?>
 
-<div class="horizontal-accordion-wrapper" id="horizontal-accordion">
-  <div class="horizontal-accordion-container">
-    <div class="horizontal-accordion-btn-group"></div>
-    <ul class="horizontal-accordion horizontal-accordion-static">
+<div class="wrapper wrapper--featured">
+  <div class="container">
+    <div class="row cards cards--4">
       <?php
       $args = array( 'posts_per_page' => 4 );
 
       $myposts = get_posts( $args );
-      foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-        <?php
+
+      foreach ( $myposts as $post ) {
+        setup_postdata( $post );
         if (has_post_thumbnail( $post->ID ) ):
           $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
         else :
           $image = array("/assets/images/frontpage.jpg");
-        endif;
-        ?>
-        <li class="horizontal-accordion-tab" style="background-image: url(<?php echo $image[0]; ?>);">
-          <a class="tab-overlay" href="<?php the_permalink(); ?>">
-            <div class="tab-content">
-              <h1><?php the_title(); ?></h1>
-              <h2 class="tab-meta">
-                <span><?php echo parse_date(get_the_date('c')); ?></span>
-                <?php
-                if ( count(get_the_category()) > 0 ) {
-                  foreach ( get_the_category() as $cat ) { ?>
-                    | <span href="<?php echo $cat->slug; ?>"><?php echo $cat->name; ?></span><?php
-                  }
-                }
-                ?>
-              </h2>
-              <p><?php wp_strip_all_tags( the_excerpt() ); ?></p>
-            </div>
-          </a>
-        </li>
-      <?php endforeach;
-      wp_reset_postdata();?>
-    </ul>
+        endif; ?>
 
-    <div class="horizontal-accordion-btn-group">
-      <div class="container">
-        <a type="button" href="<?php echo get_category_link(get_translated_category_by_slug('ajankohtaista')) ?>" class="btn btn-secondary btn--show-all">
-          <?php _e('Show all', 'sixodp');?>  <i class="material-icons">arrow_forward</i>
-        </a>
-        <a type="button" href="<?php echo get_permalink(get_translated_page_by_title('Ajankohtaista')); ?>" class="btn btn-secondary btn--ajankohtaista">
-          <?php _e('Latest updates', 'sixodp');?>  <i class="material-icons">arrow_forward</i>
-        </a>
-      </div>
+        <div class="card--image">
+          <img src="<?php echo $image[0]; ?>">
+          <div class="card-content card-content-slide-up">
+            <h4 class="card-title"><?php the_title(); ?></h4>
+            <div class="card-title-secondary"><?php echo parse_date(get_the_date('c')); ?></div>
+            <div class="card-description"><?php the_excerpt(); ?></div>
+            <div class="card-link-wrapper">
+              <a href="<?php the_permalink(); ?>"
+                 class="btn btn-transparent card-link">
+                <?php _e('Read more', 'sixodp') ?>
+              </a>
+            </div>
+          </div>
+        </div>
+      <?php } wp_reset_postdata();?>
+    </div>
+
+    <div class="row btn-container">
+      <a type="button" href="<?php echo get_category_link(get_translated_category_by_slug('ajankohtaista')) ?>" class="btn btn-transparent--inverse btn--show-all">
+        <?php _e('Show all', 'sixodp');?>
+      </a>
+      <a type="button" href="<?php echo get_permalink(get_translated_page_by_title('Ajankohtaista')); ?>" class="btn btn-transparent--inverse btn--ajankohtaista">
+        <?php _e('Latest updates', 'sixodp');?>
+      </a>
     </div>
   </div>
 </div>
