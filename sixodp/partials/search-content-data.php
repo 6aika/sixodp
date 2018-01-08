94c16d55ec2ea4df087b010c897728b8628f29f7
Mikko Nieminen <mikko.nieminen@gofore.com>
2018-01-08 15:03:24 +0200

    Improve wp search page. Update wp translations

--- sixodp/partials/search-content-data.php
+++ sixodp/partials/search-content-data.php
@@ -14,63 +14,66 @@
   $searchcount = get_posts(array('s' => $searchterm, 'post_type' => 'any' ));
   $searchcount =  count($searchcount);
 ?>
-<div class="container">
-  <div class="row">
-    <div class="search-sidebar col-sm-3">
-      <ul class="sidebar-section">
-        <li class="sidebar-heading">
-          <?php _e('Results in groups', 'sixodp');?>
-        </li>
-        <li class="sidebar-item">
-            <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>&datasearch" title=""> <span><?php _e('Datasets', 'sixodp');?>  (<?php echo $data_dataset['count']; ?>)</span></a>
-        </li>
-        <li class="sidebar-item">
-            <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>&datasearch&showcase" title=""> <span><?php _e('Applications', 'sixodp');?>  (<?php echo $data_showcase['count']; ?>)</span></a>
-        </li>
-        <li class="sidebar-item">
-            <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>" title="" class="active"> <span><?php _e('Others', 'sixodp');?>  (<?php  echo $searchcount; ?>)</span></a>
-        </li>
-      </ul>
-    </div>
-    <div class="col-md-8 search-container">
-      <?php
-        $type = 'dataset';
-        if(isset($_GET['showcase'])) {
-          $results = $data_showcase;
-          $type = 'showcase';
-        }
-        else {
-          $results = $data_dataset;
-        }
-      ?>
-      <h3 class="heading"><?php printf( esc_html( _n( 'Found %d result', 'Found %d results', $results['count'], 'sixodp' ) ), $results['count'] ); ?></h3>
-          <ul class="search-content__list">
-            <?php foreach ( $results['results'] as $result ) : ?>
-            <li class="search-content">
-              <div class="search-content__content">
-                <span class="search-content__type"><?php echo $item['type']; ?></span>
-                <h4 class="search-content__title">
-                  <a class="search-content__link" href="<?php echo CKAN_BASE_URL ?>/<?php echo get_lang(); ?>/<?php echo $type ?>/<?php echo $result['name']; ?>">
-                    <?php echo get_translated($result, 'title'); ?>
-                  </a>
-                </h4>
-                <div class="search-content__body">
-                  <div class="metadata">
-                      <span class="time">
-                        <?php
-                          if (isset($result['date_updated'])) {
-                            $date = DateTime::createFromFormat('Y-m-j', $result['date_updated']);
-                            echo $date->format('d.m.Y');
-                          }
-                        ?>
-                      </span>
-                  </div>
-                  <p class="search-content__info"><?php echo wp_trim_words( get_translated($result, 'notes'), 55, '...' ); ?></p>
+
+<div class="row">
+  <div class="search-sidebar col-md-3 col-sm-12">
+    <h3 class="heading-sidebar"><?php _e('Results in groups', 'sixodp');?></h3>
+    <ul>
+      <li class="sidebar-item">
+        <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>&datasearch" title="<?php _e('Datasets', 'sixodp');?>">
+          <?php _e('Datasets', 'sixodp');?>&nbsp;(<?php echo $data_dataset['count']; ?>)
+        </a>
+      </li>
+      <li class="sidebar-item">
+        <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>&datasearch&showcase" title="<?php _e('Applications', 'sixodp');?>">
+          <?php _e('Applications', 'sixodp');?>&nbsp;(<?php echo $data_showcase['count']; ?>)
+        </a>
+      </li>
+      <li class="sidebar-item">
+        <a href="<?php echo get_site_url(); ?>/<?php echo get_current_locale() ?>/?s=<?php echo $searchterm;?>" title="<?php _e('Others', 'sixodp');?>">
+          <?php _e('Others', 'sixodp');?>&nbsp;(<?php echo $searchcount; ?>)
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="col-md-9 col-sm-12 search-container">
+    <?php
+      $type = 'dataset';
+      if(isset($_GET['showcase'])) {
+        $results = $data_showcase;
+        $type = 'showcase';
+      }
+      else {
+        $results = $data_dataset;
+      }
+    ?>
+    <h3 class="search-results-heading"><?php printf( esc_html( _n( 'Found %d result', 'Found %d results', $results['count'], 'sixodp' ) ), $results['count'] ); ?></h3>
+        <ul class="search-content__list">
+          <?php foreach ( $results['results'] as $result ) : ?>
+          <li class="search-content">
+            <div class="search-content__content">
+              <span class="search-content__type"><?php echo $item['type']; ?></span>
+              <h4 class="search-content__title">
+                <a class="search-content__link" href="<?php echo CKAN_BASE_URL ?>/<?php echo get_lang(); ?>/<?php echo $type ?>/<?php echo $result['name']; ?>">
+                  <?php echo get_translated($result, 'title'); ?>
+                </a>
+              </h4>
+              <div class="search-content__body">
+                <div class="metadata">
+                    <span class="time">
+                      <?php
+                        if (isset($result['date_updated'])) {
+                          $date = DateTime::createFromFormat('Y-m-j', $result['date_updated']);
+                          echo $date->format('d.m.Y');
+                        }
+                      ?>
+                    </span>
                 </div>
+                <p class="search-content__info"><?php echo wp_trim_words( get_translated($result, 'notes'), 55, '...' ); ?></p>
               </div>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+            </div>
+          </li>
+          <?php endforeach; ?>
+      </ul>
   </div>
 </div>
--- sixodp/searchform.php
+++ sixodp/searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label>
-        <input type="search" class="form-control input-lg"
-            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-            value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
-    <button type="submit" class="btn btn-lg btn-primary"><?php _e('Search', 'sixodp');?></button>
+  <label>
+    <input type="search" class="form-control input-lg"
+        placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder', 'sixodp' ) ?>"
+        value="<?php echo get_search_query() ?>" name="s"
+        title="<?php echo esc_attr_x( 'Search for', 'label', 'sixodp' ) ?>" />
+  </label>
+  <button type="submit" class="btn btn-lg btn-primary"><?php _e('Search', 'sixodp');?></button>
 </form>
