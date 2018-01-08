43325e1200f982633d0db88a0433803ca7bca61e
Mikko Nieminen <mikko.nieminen@gofore.com>
2018-01-08 09:10:53 +0200

    Fix tuki-template, update translations

--- sixodp/tuki.php
+++ sixodp/tuki.php
@@ -17,43 +17,50 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main site-main--home" role="main">
-		<?php
+		<?php get_template_part('partials/page-hero'); ?>
 
-		get_template_part('partials/header-logos');
+    <div class="toolbar-wrapper">
+      <div class="toolbar">
+        <div class="container">
+          <ol class="breadcrumb">
+            <li><a href="<?php echo get_home_url() ?>"><?php _e('Home', 'sixodp') ?></a></li>
+            <li><a href="<?php echo home_url( $wp->request ) ?>"><?php _e('Support', 'sixodp') ?></a></li>
+          </ol>
+        </div>
+      </div>
+      <div class="toolbar--site-subtitle">
+        <h1><?php _e('Support', 'sixodp') ?></h1>
+      </div>
+    </div>
 
-		echo '<h1 class="heading--tuki">Tuki</h1>';
-		get_template_part( 'partials/tuki-contentbox' );
-
-		$morelinks_title  = "Teemat";
-    $teema_category = get_translated_category_by_slug('teemat');
-        ?>
-        <div class="wrapper--morelinks">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-4">
-                <h1 class="heading--themes"><?php _e('Themes') ?></h1>
+    <?php
+      get_template_part( 'partials/tuki-contentbox' );
+      $teema_category = get_translated_category_by_slug('teemat');
+    ?>
+    <div class="wrapper--morelinks">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h1 class="heading--themes"><?php _e('Themes', 'sixodp') ?></h1>
+          </div>
+          <div class="col-md-8">
+            <?php foreach (get_categories(array('parent' => $teema_category->term_id, 'hide_empty' => false)) as $category) : ?>
+              <div class="icon-link morelink">
+                <p class="icon-link__icon">
+                  <a class="icon-link__link--round" href="<?php echo get_category_link($category->term_id); ?>">
+                    <i class="material-icons">arrow_forward</i>
+                  </a>
+                </p>
+                <p class="icon-link__text">
+                  <a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></a>
+                </p>
               </div>
-              <div class="col-md-8">
-                <?php foreach (get_categories(array('parent' => $teema_category->term_id, 'hide_empty' => false)) as $category) : ?>
-                  <div class="icon-link morelink">
-                    <p class="icon-link__icon">
-                      <a class="icon-link__link--round" href="<?php echo get_category_link($category->term_id); ?>">
-                        <i class="material-icons">arrow_forward</i>
-                      </a>
-                    </p>
-                    <p class="icon-link__text">
-                      <a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></a>
-                    </p>
-                  </div>
-                <?php endforeach; ?>
-              </div>
-            </div>
+            <?php endforeach; ?>
           </div>
         </div>
-        <?php
-		get_template_part( 'partials/tuki-contactbanner' );
-
-		?>
+      </div>
+    </div>
+    <?php get_template_part( 'partials/tuki-contactbanner' ); ?>
 
 	</main><!-- .site-main -->
 
