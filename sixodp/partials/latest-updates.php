444d0be534522d9945fdbe5553be240cb1484310
Mikko Nieminen <mikko.nieminen@gofore.com>
2018-07-18 15:05:12 +0300

    Add navbar shadow, add additional latest updates heading to home, add card accordion for latest updates cards in mobile

--- sixodp/partials/latest-updates.php
+++ sixodp/partials/latest-updates.php
@@ -10,9 +10,72 @@
   </div>
 
   <div class="container">
-    <div class="row cards cards--4">
-      <?php foreach ( get_latest_updates(array(), false, 8) as $index => $updated_item ) : ?>
-        <?php if ($index % 4 === 0) echo '</div><div class="row cards cards--4">'; ?>
+    <?php $updated_items = get_latest_updates(array(), false, 8); ?>
+
+    <div id="latest-updates-carousel" class="carousel slide mobile-only" data-ride="carousel" data-interval="false">
+      <div class="carousel-inner" role="listbox">
+        <?php foreach($updated_items as $updated_item) : ?>
+          <?php $extra_classes = $updated_item === reset($updated_items) ? ' active' : ''; ?>
+
+          <div class="item<?php echo $extra_classes ?>">
+            <?php
+              $label = is_array($updated_item['type']) ? $updated_item['type']['label'] : $updated_item['type'];
+              $meta_label = '';
+              switch ($label) {
+                case 'dataset':
+                  $meta_label = __('Dataset', 'sixodp');
+                  break;
+                case 'showcase':
+                  $meta_label = __('Showcase', 'sixodp');
+                  break;
+                case 'post':
+                  $meta_label = __('Article', 'sixodp');
+                  break;
+                case 'page':
+                  $meta_label = __('Page', 'sixodp');
+                  break;
+                case 'comment':
+                  $meta_label = __('Comment', 'sixodp');
+                  break;
+                default:
+                  $meta_label = $label;
+                  break;
+              }
+
+              $item = array(
+                'external_card_class' => 'card-danger',
+                'title' => get_translated($updated_item, 'title'),
+                'meta' => $meta_label,
+                'timestamp' => $updated_item['date_recent'],
+                'notes' => get_translated($updated_item, 'notes'),
+                'url' => $updated_item['link'],
+              );
+              include(locate_template( 'partials/card.php' ));
+            ?>
+          </div>
+        <?php endforeach; ?>
+      </div>
+
+      <a class="left carousel-control" href="#latest-updates-carousel" role="button" data-slide="prev">
+        <span class="fa fa-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#latest-updates-carousel" role="button" data-slide="next">
+        <span class="fa fa-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+
+      <ol class="carousel-indicators">
+        <?php foreach($updated_items as $index => $updated_item) : ?>
+          <?php $extra_classes = $updated_item === reset($updated_items) ? 'active' : ''; ?>
+          <li data-target="#latest-updates-carousel" data-slide-to="<?php echo $index ?>" class="<?php echo $extra_classes ?>"></li>
+        <?php endforeach; ?>
+      </ol>
+    </div>
+
+    <div class="row cards cards--4 desktop-only">
+      <?php foreach ( $updated_items as $index => $updated_item ) : ?>
+        <?php if ($index % 4 === 0) echo '</div><div class="row cards cards--4 desktop-only">'; ?>
         <?php
           $label = is_array($updated_item['type']) ? $updated_item['type']['label'] : $updated_item['type'];
           $meta_label = '';
