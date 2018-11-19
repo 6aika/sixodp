8ec13aa5031e60613fd2d597d80537f3dd195c84
Jari Voutilainen <jari.voutilainen@iki.fi>
2018-11-19 12:26:19 +0200

    Fix layout of archive pages

--- sixodp/archive-data_request.php
+++ sixodp/archive-data_request.php
@@ -37,7 +37,7 @@ get_header(); ?>
         <div class="row">
           <?php $data_req_url = get_permalink(get_translated_page_by_title('Uusi datatoive')); ?>
           <div class="news-content">
-            <div class="col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-md-8 col-sm-8 col-xs-8">
+            <div class="col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-md-8 col-sm-8 col-xs-8 btn-container-inverse">
               <a href="<?php echo $data_req_url; ?>" class="btn btn-transparent--inverse">
                 <?php _e('New data request', 'sixodp') ?>
               </a>
--- sixodp/archive-showcase_idea.php
+++ sixodp/archive-showcase_idea.php
@@ -35,7 +35,7 @@ get_header(); ?>
       <div class="wrapper">
         <div class="row">
           <div class="news-content">
-            <div class="col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-md-8 col-sm-8 col-xs-8">
+            <div class="col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-md-8 col-sm-8 col-xs-8 btn-container-inverse">
               <?php $showcase_idea = get_permalink(get_translated_page_by_title('Uusi sovellusidea')); ?>
               <a href="<?php echo $showcase_idea; ?>" class="btn btn-transparent--inverse">
                 <?php _e('New showcase idea', 'sixodp') ?>
