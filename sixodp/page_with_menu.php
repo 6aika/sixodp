a9e1307b69f548dbd254463bacc642942d42c96c
Jari Voutilainen <jari.voutilainen@iki.fi>
2018-11-27 12:30:44 +0200

    Add breadcrumbs of parent to template

--- sixodp/page_with_menu.php
+++ sixodp/page_with_menu.php
@@ -28,6 +28,12 @@ $category = $post;
         <div class="container">
           <ol class="breadcrumb">
             <li><a href="<?php echo get_home_url() ?>"><?php _e('Home', 'sixodp') ?></a></li>
+              <?php if ($parent_page && $parent_page != $post)  : ?>
+                  <li><a href="<?php echo get_permalink($parent_page); ?>"><?php echo get_the_title( $parent_page); ?></a></li>
+              <?php endif;?>
+              <?php if ($post->post_parent && $post->post_parent != $grandparent_id) : ?>
+              <li><a href="<?php echo get_permalink($post->post_parent); ?>"><?php echo get_the_title( $post->post_parent ); ?></a></li>
+              <?php endif;?>
             <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
           </ol>
         </div>
