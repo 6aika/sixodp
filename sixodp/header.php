18a1d0e4a44945dbdc2e30f05dbb55218b1caec6
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-09-01 10:41:26 +0300

    Accessibility fixes to hero and header elements

--- sixodp/header.php
+++ sixodp/header.php
@@ -61,7 +61,7 @@
     <?php require_once('partials/nav.php'); ?>
   </div>
 
-  <div class="logos">
+  <div class="logos" role="banner">
     <a href="<?php echo site_url(); ?>" class="logo--brand">
       <img src="<?php echo assets_url(); ?>/images/hero_logo.png" />
     </a>
