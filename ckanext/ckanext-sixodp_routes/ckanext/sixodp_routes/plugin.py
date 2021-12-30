ac1047ec63e0652806faaa889d46531a68b86f0b
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-12-30 13:36:50 +0200

    Sort by relevance when text search is used

--- ckanext/ckanext-sixodp_routes/ckanext/sixodp_routes/plugin.py
+++ ckanext/ckanext-sixodp_routes/ckanext/sixodp_routes/plugin.py
@@ -2,7 +2,7 @@ from pylons import config
 import ckan
 from ckan.controllers.revision import RevisionController
 from ckan.controllers.organization import OrganizationController
-from ckan.controllers.user import UserController
+from ckan.controllers.group import GroupController
 from ckan.controllers.package import PackageController, search_url, _encode_params
 from ckan.common import c, _, request, OrderedDict, g
 import ckan.model as model
@@ -17,6 +17,7 @@ import ckan.lib.base as base
 import ckan.lib.mailer as mailer
 from ckan.plugins import toolkit
 import ckan.plugins as p
+from six import string_types
 
 from ckan.controllers.user import set_repoze_user
 
@@ -68,6 +69,9 @@ class Sixodp_RoutesPlugin(ckan.plugins.SingletonPlugin):
                   controller='ckanext.sixodp_routes.plugin:Sixodp_OrganizationController')
         m.connect('search', '/dataset', action='search',
                   highlight_actions='index search', controller='ckanext.sixodp_routes.plugin:Sixodp_PackageController')
+
+        m.connect('group_read', '/group/{id}', action='read',
+                  controller='ckanext.sixodp_routes.plugin:Sixodp_GroupController')
 
         m.connect('/api/2/util/tag/autocomplete', action='tag_autocomplete', controller='ckanext.sixodp_routes.controller:Sixodp_RoutesController')
         return m
@@ -169,6 +173,14 @@ class Sixodp_OrganizationController(OrganizationController):
             return search_url(params)
 
         try:
+
+            # Set the custom default sort parameter here
+            # Might need a rewrite when ckan is updated
+            if q and not sort_by:
+                sort_by = 'score desc, metadata_modified desc'
+            elif not q and not sort_by:
+                sort_by = 'date_released desc'
+
             c.fields = []
             search_extras = {}
             for (param, value) in request.params.items():
@@ -215,11 +227,6 @@ class Sixodp_OrganizationController(OrganizationController):
                 del facets['capacity']
 
             c.facet_titles = facets
-
-            # Set the custom default sort parameter here
-            # Might need a rewrite when ckan is updated
-            if not sort_by:
-                sort_by = 'date_released desc'
 
             data_dict = {
                 'q': q,
@@ -393,8 +400,14 @@ class Sixodp_PackageController(PackageController):
 
             # Set the custom default sort parameter here
             # Might need a rewrite when ckan is updated
-            if not sort_by:
+            log.info(q)
+            log.info(sort_by)
+            if q and not sort_by:
+                sort_by = 'score desc, metadata_modified desc'
+            elif not sort_by:
                 sort_by = 'metadata_created desc'
+
+            log.info(sort_by)
 
             data_dict = {
                 'q': q,
@@ -454,6 +467,175 @@ class Sixodp_PackageController(PackageController):
         return render(self._search_template(package_type),
                       extra_vars={'dataset_type': package_type})
 
+
+class Sixodp_GroupController(GroupController):
+    def read(self, id, limit=20):
+        group_type = self._ensure_controller_matches_group_type(
+            id.split('@')[0])
+
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user,
+                   'schema': self._db_to_form_schema(group_type=group_type),
+                   'for_view': True}
+        data_dict = {'id': id, 'type': group_type}
+
+        # unicode format (decoded from utf8)
+        c.q = request.params.get('q', '')
+
+        try:
+            # Do not query for the group datasets when dictizing, as they will
+            # be ignored and get requested on the controller anyway
+            data_dict['include_datasets'] = False
+            c.group_dict = self._action('group_show')(context, data_dict)
+            c.group = context['group']
+        except (NotFound, NotAuthorized):
+            abort(404, _('Group not found'))
+
+        self._read(id, limit, group_type)
+        return render(self._read_template(c.group_dict['type']),
+                      extra_vars={'group_type': group_type})
+
+    def _read(self, id, limit, group_type):
+        ''' This is common code used by both read and bulk_process'''
+        context = {'model': model, 'session': model.Session,
+                   'user': c.user,
+                   'schema': self._db_to_form_schema(group_type=group_type),
+                   'for_view': True, 'extras_as_string': True}
+
+        q = c.q = request.params.get('q', '')
+        # Search within group
+        if c.group_dict.get('is_organization'):
+            fq = 'owner_org:"%s"' % c.group_dict.get('id')
+        else:
+            fq = 'groups:"%s"' % c.group_dict.get('name')
+
+        c.description_formatted = \
+            h.render_markdown(c.group_dict.get('description'))
+
+        context['return_query'] = True
+
+        page = h.get_page_number(request.params)
+
+        # most search operations should reset the page counter:
+        params_nopage = [(k, v) for k, v in request.params.items()
+                         if k != 'page']
+        sort_by = request.params.get('sort', None)
+
+        def search_url(params):
+            controller = lookup_group_controller(group_type)
+            action = 'bulk_process' if c.action == 'bulk_process' else 'read'
+            url = h.url_for(controller=controller, action=action, id=id)
+            params = [(k, v.encode('utf-8') if isinstance(v, string_types)
+            else str(v)) for k, v in params]
+            return url + u'?' + urlencode(params)
+
+        def drill_down_url(**by):
+            return h.add_url_param(alternative_url=None,
+                                   controller='group', action='read',
+                                   extras=dict(id=c.group_dict.get('name')),
+                                   new_params=by)
+
+        c.drill_down_url = drill_down_url
+
+        def remove_field(key, value=None, replace=None):
+            controller = lookup_group_controller(group_type)
+            return h.remove_url_param(key, value=value, replace=replace,
+                                      controller=controller, action='read',
+                                      extras=dict(id=c.group_dict.get('name')))
+
+        c.remove_field = remove_field
+
+        def pager_url(q=None, page=None):
+            params = list(params_nopage)
+            params.append(('page', page))
+            return search_url(params)
+
+        try:
+
+            # Set the custom default sort parameter here
+            # Might need a rewrite when ckan is updated
+            if q and not sort_by:
+                sort_by = 'score desc, metadata_modified desc'
+            elif not sort_by:
+                sort_by = 'metadata_created desc'
+
+            c.fields = []
+            c.fields_grouped = {}
+            search_extras = {}
+            for (param, value) in request.params.items():
+                if param not in ['q', 'page', 'sort'] \
+                        and len(value) and not param.startswith('_'):
+                    if not param.startswith('ext_'):
+                        c.fields.append((param, value))
+                        q += ' %s: "%s"' % (param, value)
+                        if param not in c.fields_grouped:
+                            c.fields_grouped[param] = [value]
+                        else:
+                            c.fields_grouped[param].append(value)
+                    else:
+                        search_extras[param] = value
+
+            facets = OrderedDict()
+
+            default_facet_titles = {'organization': _('Organizations'),
+                                    'groups': _('Groups'),
+                                    'tags': _('Tags'),
+                                    'res_format': _('Formats'),
+                                    'license_id': _('Licenses')}
+
+            for facet in h.facets():
+                if facet in default_facet_titles:
+                    facets[facet] = default_facet_titles[facet]
+                else:
+                    facets[facet] = facet
+
+            # Facet titles
+            facets = self._update_facet_titles(facets, group_type)
+
+            c.facet_titles = facets
+
+            data_dict = {
+                'q': q,
+                'fq': fq,
+                'include_private': True,
+                'facet.field': facets.keys(),
+                'rows': limit,
+                'sort': sort_by,
+                'start': (page - 1) * limit,
+                'extras': search_extras
+            }
+
+            context_ = dict((k, v) for (k, v) in context.items()
+                            if k != 'schema')
+            query = get_action('package_search')(context_, data_dict)
+
+            c.page = h.Page(
+                collection=query['results'],
+                page=page,
+                url=pager_url,
+                item_count=query['count'],
+                items_per_page=limit
+            )
+
+            c.group_dict['package_count'] = query['count']
+
+            c.search_facets = query['search_facets']
+            c.search_facets_limits = {}
+            for facet in c.search_facets.keys():
+                limit = int(request.params.get('_%s_limit' % facet,
+                                               config.get('search.facets.default', 10)))
+                c.search_facets_limits[facet] = limit
+            c.page.items = query['results']
+
+            c.sort_by_selected = sort_by
+
+        except search.SearchError as se:
+            log.error('Group search error: %r', se.args)
+            c.query_error = True
+            c.page = h.Page(collection=[])
+
+        self._setup_template_variables(context, {'id': id},
+                                       group_type=group_type)
 
 class Sixodp_RevisionController(RevisionController):
 
