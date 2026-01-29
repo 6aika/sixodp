8410e4f101e3b9a7085a44b606089f96a2a0ec9e
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2026-01-29 09:12:45 +0000

    Convert stub object to vars

--- ckanext/ckanext-sixodp/ckanext/sixodp/tests/test_plugin.py
+++ ckanext/ckanext-sixodp/ckanext/sixodp/tests/test_plugin.py
@@ -5,5 +5,5 @@ from ckan.tests.helpers import call_action
 @pytest.mark.usefixtures('clean_db', 'clean_index')
 class TestSixodpPlugin():
     def test_mandatory_values_in_dataset(self):
-        dataset = SixodpDataset.stub()
+        dataset = vars(SixodpDataset.stub())
         result = call_action('package_create', **dataset)
