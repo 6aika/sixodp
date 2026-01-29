30454cb27579c7d2ee71565316a8568b898dfaf6
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2026-01-29 12:21:02 +0000

    Update working tests

--- ckanext/ckanext-sixodp/ckanext/sixodp/tests/factories.py
+++ ckanext/ckanext-sixodp/ckanext/sixodp/tests/factories.py
@@ -7,6 +7,16 @@ from faker import Faker
 fake = Faker()
 
 class SixodpDataset(Dataset):
-    title_translated = factory.Dict({
-        'fi': factory.LazyFunction(lambda: fake.sentence(nb_words=5))
-    })
+    title_translated = {
+        'fi': fake.sentence(nb_words=5)
+    }
+    notes_translated = {
+        'fi': fake.sentence(nb_words=5)
+    }
+    date_released = factory.LazyFunction(fake.date)
+    geographical_coverage = factory.LazyFunction(lambda: fake.words(nb=5))
+    keywords = {
+        'fi': ['somekeyword']
+    }
+    license_id = 'cc-by-40'
+    maintainer_email = factory.LazyFunction(lambda: fake.email())
